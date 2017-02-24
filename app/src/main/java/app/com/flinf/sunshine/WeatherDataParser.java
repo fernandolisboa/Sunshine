984276e9600d422cb759cf8bcc9c2a75b3d23a5f
Fernando <fernandoigorlisboa@gmail.com>
2017-02-24 02:16:40 -0300

    leitura e tratamento do JSON

---
+++ app/src/main/java/app/com/flinf/sunshine/WeatherDataParser.java
@@ -0,0 +1,29 @@
+package app.com.flinf.sunshine;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by Fernando on 24/02/2017.
+ * Desenvolvendo o App Sunshine do curso da Udacity
+ */
+
+public class WeatherDataParser {
+    /**
+     * Given a string of the form returned by the api call:
+     * http://api.openweathermap.org/data/2.5/forecast/daily?q=71050171%2CBR&mode=json&units=metric&cnt=7
+     * retrieve the maximum temperature for the day indicated by dayIndex
+     * (Note: 0-indexed, so 0 would refer to the first day).
+     */
+    public static double getMaxTemperatureForDay(String weatherJsonStr, int dayIndex)
+            throws JSONException {
+
+        JSONObject weather = new JSONObject(weatherJsonStr);
+        JSONArray days = weather.getJSONArray("list");
+        JSONObject dayInfo = days.getJSONObject(dayIndex);
+        JSONObject temperatureInfo = dayInfo.getJSONObject("temp");
+        return temperatureInfo.getDouble("max");
+    }
+
+}
